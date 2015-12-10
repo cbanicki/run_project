daffec0225d310623e6ff28b83ee9fc064a83ba8
Chad Banicki <cbanicki@homeaway.com>
2015-12-10 14:45:29 -0600

    merging together data, having issues getting the activity names to merge

--- run_analysis.R
+++ run_analysis.R
@@ -69,11 +69,15 @@ run_analysis <- function() {
   
   activity_labels <- fread("UCI HAR Dataset//activity_labels.txt")
   
+  colnames(activity_labels) <- c("ActivityCode","ActivityName")
+  
   features <- fread("UCI HAR Dataset//features.txt")
   
   #transform the column headers to one row so they can be used as the header
   
   features <- t(features)
+  
+  #head(activity_labels)
   
 
   
@@ -81,47 +85,51 @@ run_analysis <- function() {
   #                                           Test Section
   #####################################################################################################
     
-#   x_test <- fread("UCI HAR Dataset//test//X_test.txt")
-#   
-#  
-#   # Add a row number column for reference
-#   
-#   x_test$RowId <- 1:nrow(x_test)
-#   
-#   
-#   
-#   # Get activity codes for Test
-#   
-#      y_test <- fread("UCI HAR Dataset//test//y_test.txt")
-#      
-#      #colnames(y_test) <- "ActivityCode"
-# 
-#      y_test$RowId <- 1:nrow(y_test)
-#      
-#      
-#   # Merge the activity codes into the Test data set 
-#   x_test <- merge(x_test, y_test, by="RowId")
-#   
-#   
-#   # Get Volunteer codes for Test
-#   subject_test <- fread("UCI HAR Dataset//test//subject_test.txt")  
-#   
-#   
-#   subject_test$RowId <- 1:nrow(subject_test)
-#     
-#    #colnames(subject_test) <- "Subject"
-#   
-#   
-#   # Merge the subject codes into the Test data set 
-#   x_test <- merge(x_test, subject_test, by="RowId")
-#   
-#   #Add feature column names to Test
-#   colnames(x_test) <- c(features[2,])
-#   
-#   #New Column Just to identify the Source of data in table
-#   x_test$Source <- rep("Test",nrow(x_test)) 
-#   
+  x_test <- fread("UCI HAR Dataset//test//X_test.txt")
+  
  
+  # Add a row number column for reference
+  
+  x_test$RowId <- 1:nrow(x_test)
+  
+  
+  
+  # Get activity codes for Test
+  
+     y_test <- fread("UCI HAR Dataset//test//y_test.txt")
+     
+     #colnames(y_test) <- "ActivityCode"
+
+     y_test$RowId <- 1:nrow(y_test)
+     
+     
+  # Merge the activity codes into the Test data set 
+  x_test <- merge(x_test, y_test, by="RowId")
+  
+  
+  # Get Volunteer codes for Test
+  subject_test <- fread("UCI HAR Dataset//test//subject_test.txt")  
+  
+  
+  subject_test$RowId <- 1:nrow(subject_test)
+    
+
+    # Merge the subject codes into the Test data set 
+  x_test <- merge(x_test, subject_test, by="RowId")
+  
+  #New Column Just to identify the Source of data in table
+  x_test$Source <- rep("Test",nrow(x_test)) 
+  
+  #Add feature column names to Test
+  colnames(x_test) <- c("RowId",features[2,],"ActivityCode","SubjectCode","DataSource")
+  
+#   # Merge the Activty Names into the train data set 
+#   x_test <- merge(x_test, activity_labels, by="ActivityCode")
+#   
+  
+  
+  
+  # head(x_test)
   
 # ########################################################################################################
 #                                         Train Section
@@ -160,12 +168,13 @@ run_analysis <- function() {
   
   subject_train$RowId <- 1:nrow(subject_train)
   
-  #colnames(subject_train) <- "Subject"
-  
-  
+
   # Merge the subject codes into the train data set 
   x_train <- merge(x_train, subject_train, by="RowId")
   
+  
+#   # Merge the Activty Names into the train data set 
+#   x_train <- merge(x_train, activity_labels, by="ActivityCode")
 
   
   #New Column Just to identify the Source of data in table
@@ -173,15 +182,30 @@ run_analysis <- function() {
   
   
   #Add feature column names to Train
-  colnames(x_train) <- c(features[2,],"RowId","ActivityCode","SubjectCode","DataSource")
+  colnames(x_train) <- c("RowId",features[2,],"ActivityCode","SubjectCode","DataSource")
   
-   head(x_train)
   
-############################################################################################################
+#   # Merge the Activty Names into the train data set 
+#   x_train <- merge(x_train,activity_labels, by="ActivityCode")
+  
+  
+  
+  # head(x_train)
   
 
+  # ########################################################################################################
+  #                                         Combine Test and Train 
+  # ########################################################################################################   
   
-
+  x_combined <- rbind.data.frame(x_test,x_train)
+  
+  # Merge the subject codes into the train data set 
+  #x_combined <- merge(x_combined, activity_labels, by.x_combined="ActivityCode", by.activity_lables="ActivityCode", all = TRUE)
+  
+  
+  
+  head(x_combined)
+  
 }
 
 
