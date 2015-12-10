181d910fd14ad8d53e0712774d664eacb2a587d5
Chad Banicki <cbanicki@homeaway.com>
2015-12-10 13:55:27 -0600

    split the processing up into  two  main sections, and added merges for activities and subjects

--- run_analysis.R
+++ run_analysis.R
@@ -22,6 +22,14 @@ run_analysis <- function() {
   setwd("C://R//Data//Project")
   
 
+  
+  
+  
+  #####################################################################################################
+  #                                           Collect Data
+  #####################################################################################################
+  
+  
   #check whether you have already dowloaded the data, then download and unzip if not 
   if (!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
       
@@ -55,79 +63,123 @@ run_analysis <- function() {
   
   require(data.table)
   
-#   activity_labels <- fread("UCI HAR Dataset//activity_labels.txt")
+  
+  
+  # Format activity labels and features data to be used in final data set
+  
+  activity_labels <- fread("UCI HAR Dataset//activity_labels.txt")
+  
+  features <- fread("UCI HAR Dataset//features.txt")
+  
+  #transform the column headers to one row so they can be used as the header
+  
+  features <- t(features)
+  
+
+  
+  #####################################################################################################
+  #                                           Test Section
+  #####################################################################################################
+    
+#   x_test <- fread("UCI HAR Dataset//test//X_test.txt")
 #   
-   features <- fread("UCI HAR Dataset//features.txt")
-  
-   #transform the column headers to one row so they can be used as the header
-   
-   features <- t(features)
+#  
+#   # Add a row number column for reference
 #   
-#   subject_test <- fread("UCI HAR Dataset//test//subject_test.txt")
+#   x_test$RowId <- 1:nrow(x_test)
 #   
-#   subject_train <- fread("UCI HAR Dataset//test//subject_train.txt")
 #   
-  x_test <- fread("UCI HAR Dataset//test//X_test.txt")
+#   
+#   # Get activity codes for Test
+#   
+#      y_test <- fread("UCI HAR Dataset//test//y_test.txt")
+#      
+#      #colnames(y_test) <- "ActivityCode"
+# 
+#      y_test$RowId <- 1:nrow(y_test)
+#      
+#      
+#   # Merge the activity codes into the Test data set 
+#   x_test <- merge(x_test, y_test, by="RowId")
+#   
+#   
+#   # Get Volunteer codes for Test
+#   subject_test <- fread("UCI HAR Dataset//test//subject_test.txt")  
+#   
+#   
+#   subject_test$RowId <- 1:nrow(subject_test)
+#     
+#    #colnames(subject_test) <- "Subject"
+#   
+#   
+#   # Merge the subject codes into the Test data set 
+#   x_test <- merge(x_test, subject_test, by="RowId")
+#   
+#   #Add feature column names to Test
+#   colnames(x_test) <- c(features[2,])
+#   
+#   #New Column Just to identify the Source of data in table
+#   x_test$Source <- rep("Test",nrow(x_test)) 
+#   
+ 
   
-  colnames(x_test) <- c(features[2,])
+# ########################################################################################################
+#                                         Train Section
+# ######################################################################################################## 
+
   
-  # Add a row number column for reference
   
-  x_test$RowId <- 1:nrow(x_test)
+  x_train <- fread("UCI HAR Dataset//train//X_train.txt")
   
-  #y_test <- fread("UCI HAR Dataset//test//y_test.txt")
+  #colnames(x_train) <- c(features[2,])
   
-  # y_test$RowId <- 1:nrow(y_test)
+
   
-  x_train <- fread("UCI HAR Dataset//train//x_train.txt")
   
   # Add a row number column for reference
   
   x_train$RowId <- 1:nrow(x_train)
   
-  # y_train <- fread("UCI HAR Dataset//train//y_train.txt")
   
   
- # y_train$RowId <- 1:nrow(y_train)
+  # Get activity codes for train
   
-  #x <- c(x_test,x_test)
+  y_train <- fread("UCI HAR Dataset//train//y_train.txt")
+
   
-   x_combined = rbind(x_test,x_test)
-   
-    
-   head(x_combined)
- 
-#   
-#   y_combined <- rbind(data y_test, data y_train)
-#   
+  y_train$RowId <- 1:nrow(y_train)
   
   
-#   activity_labels <- read.table("UCI HAR Dataset//activity_labels.txt", sep = " ", header=FALSE, na.strings = NA)
-#   
-#   features <- read.table("UCI HAR Dataset//features.txt", sep = " ", header=FALSE, na.strings = NA)
-#   
-#   subject_test <- read.table("UCI HAR Dataset//test//subject_test.txt", sep = " ", header=FALSE, na.strings = NA)
-#   
-#   x_test <- read.table("UCI HAR Dataset//test//X_test.txt", colClasses = c(NULL,rep("numeric", count.fields("UCI HAR Dataset//test//X_test.txt")[1] -1 )), sep = " ", header=FALSE, na.strings = NA)
-#   
-#   y_test <- read.table("UCI HAR Dataset//test//y_test.txt", sep = " ", header=FALSE, na.strings = NA)
-#   
-#   subject_train <- read.table("UCI HAR Dataset//test//subject_train.txt", sep = " ", header=FALSE, na.strings = NA)
-#   
-#   x_train <- read.table("UCI HAR Dataset//test//x_train.txt", sep = " ", header=FALSE, na.strings = NA)
-#   
-#   y_train <- read.table("UCI HAR Dataset//test//y_train.txt", sep = " ", header=FALSE, na.strings = NA)
-  
-#   head(activity_labels, 10)
-#   head(features, 10)
-#   head(subject_test, 10)
-#   head(x_test, 10)
-#   head(y_test, 10)
-#   head(x_train, 10)
-#   head(y_train, 10)
+  # Merge the activity codes into the train data set 
+  x_train <- merge(x_train, y_train, by="RowId")
   
   
-  # Append column names to each dataset
+  # Get Volunteer codes for train
+  subject_train <- fread("UCI HAR Dataset//train//subject_train.txt")  
+  
+  
+  subject_train$RowId <- 1:nrow(subject_train)
+  
+  #colnames(subject_train) <- "Subject"
+  
+  
+  # Merge the subject codes into the train data set 
+  x_train <- merge(x_train, subject_train, by="RowId")
+  
+
+  
+  #New Column Just to identify the Source of data in table
+  x_train$Source <- rep("Train",nrow(x_train)) 
+  
+  
+  #Add feature column names to Train
+  colnames(x_train) <- c(features[2,],"RowId","ActivityCode","SubjectCode","DataSource")
+  
+   head(x_train)
+  
+############################################################################################################
+  
+
   
 
 }
