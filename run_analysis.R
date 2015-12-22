d0f56124fc93b1d6f29440cf6cf45009c25f69af
Chad Banicki <cbanicki@homeaway.com>
2015-12-21 21:02:38 -0600

    formatting changes, and added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,7 @@
 
 run_analysis <- function() {
   
-#   Might consider going back and utilizing these packages similar to this blog example, https://cran.r-project.org/web/packages/tidyr/vignettes/tidy-data.html
-  
-#   library(tidyr)
-#   library(dplyr)
-  
+
   # http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
   # 
   # Here are the data for the project: 
@@ -24,20 +20,17 @@ run_analysis <- function() {
   
   
   
-  # Interesting thing to check... do we need to compute averages grouped by?
-  # Turn this on for testing but remove before publishing
-  setwd("C://R//Data//Project")
+  # Used just for testing, comment out
+   setwd("C://R//Data//Project")
   
 
-  
-  
-  
   #####################################################################################################
   #                                           Collect Data
   #####################################################################################################
   
   
-  #check whether you have already dowloaded the data, then download and unzip if not 
+  #Check whether you have already dowloaded the data into the working director, and if not, download and unzip 
+  
   if (!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
       
       fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -46,47 +39,31 @@ run_analysis <- function() {
       
       download.file(fileURL, fileName, mode = "wb")
       
-      #download.file(fileURL,destfile="Q1q3.xlsx",mode="wb")
-      
-      #xData <- getURL(fileURL, ssl.verifypeer = FALSE)
-      
       dateDownloaded <- date()
-      
-     # print("didn't exist!")
       
       unzip(fileName, files = NULL, list = FALSE, overwrite = TRUE,
             junkpaths = FALSE, exdir = ".", unzip = "internal",
             setTimes = FALSE)
-    
-      
     }
     
-#   else {
-#     
-# 
-#     print("file existed!")
-#     
-#   }
-  
+
   require(data.table)
   
   
-  
-  # Read Activity Labels into  data frame
-  
+  # Collect activity labels
   activity_labels <- fread("UCI HAR Dataset//activity_labels.txt")
   
+ 
   # Add column names for activity labels
-  
   colnames(activity_labels) <- c("ActivityCode","ActivityName")
   
+  
+  # Collect features
   features <- fread("UCI HAR Dataset//features.txt")
   
-  #transform the column headers to one row so they can be used as the header
   
+  # Transform the column headers to one row so they can be used as the header
   features <- t(features)
-  
-  #head(activity_labels)
   
 
   
@@ -94,25 +71,27 @@ run_analysis <- function() {
   #                                           Test Section
   #####################################################################################################
     
+  
+  # Collect the x_test data
   x_test <- fread("UCI HAR Dataset//test//X_test.txt")
   
  
-  # Add a row number column for reference
-  
+  # Add a row number column for a reference key
   x_test$RowId <- 1:nrow(x_test)
   
   
-  
-  # Get activity codes for Test
-  
-     y_test <- fread("UCI HAR Dataset//test//y_test.txt")
+  # Get activity codes for Test and add a column name
+  y_test <- fread("UCI HAR Dataset//test//y_test.txt")
      
-     colnames(y_test) <- "ActivityCode"
-
-     y_test$RowId <- 1:nrow(y_test)
+  colnames(y_test) <- "ActivityCode"
      
+     
+  # Add the row number key to y_test
+  y_test$RowId <- 1:nrow(y_test)
+     
+  
   #Add activity label to y_test   
-   y_test <- merge(y_test, activity_labels, by="ActivityCode")   
+  y_test <- merge(y_test, activity_labels, by="ActivityCode")   
      
   # Merge the activity codes into the Test data set 
   x_test <- merge(x_test, y_test, by="RowId")
@@ -121,112 +100,73 @@ run_analysis <- function() {
   # Get Volunteer codes for Test
   subject_test <- fread("UCI HAR Dataset//test//subject_test.txt")  
   
-  
+  #Add key to subject_test to identify rows
   subject_test$RowId <- 1:nrow(subject_test)
     
-
-    # Merge the subject codes into the Test data set 
+  # Merge the subject codes into the Test data set 
   x_test <- merge(x_test, subject_test, by="RowId")
   
-  #New Column Just to identify the Source of data in table
+  #New key Column Just to identify the Source of data in table
   x_test$Source <- rep("Test",nrow(x_test)) 
   
   #Add feature column names to Test
   colnames(x_test) <- c("RowId",features[2,],"ActivityCode","ActivityLabel","SubjectCode","DataSource")
   
-#   # Merge the Activty Names into the train data set 
-#   x_test <- merge(x_test, activity_labels, by="ActivityCode")
-#   
-  
-  
-  
-  # head(x_test)
   
 # ########################################################################################################
 #                                         Train Section
 # ######################################################################################################## 
 
   
-  
+  # Collect the x_train data
   x_train <- fread("UCI HAR Dataset//train//X_train.txt")
   
-  #colnames(x_train) <- c(features[2,])
-  
-
-  
-  
-  # Add a row number column for reference
-  
+  # Add a row number column for a reference key
   x_train$RowId <- 1:nrow(x_train)
   
-  
-  
-  # Get activity codes for train
-  
-  
+  # Get activity codes for y_train
   y_train <- fread("UCI HAR Dataset//train//y_train.txt")
 
   
   # Add feature column names to y_train so that it can be joined to activity names
   colnames(y_train) <- c("ActivityCode")
   
+  # Add a reference key to y_train
   y_train$RowId <- 1:nrow(y_train)
   
-  # 
+  # Merge activity labels with y_train
   y_train <- merge(y_train, activity_labels, by="ActivityCode")
   
-  # Merge the activity codes into the train data set 
+  # Merge x_train and y_train 
   x_train <- merge(x_train, y_train, by="RowId")
-  
-
   
   # Get Volunteer codes for train
   subject_train <- fread("UCI HAR Dataset//train//subject_train.txt")  
   
-  
+  # Add a row key to subject_train
   subject_train$RowId <- 1:nrow(subject_train)
   
-
   # Merge the subject codes into the train data set 
   x_train <- merge(x_train, subject_train, by="RowId")
   
-  
-#   # Merge the Activty Names into the train data set 
-#   x_train <- merge(x_train, activity_labels, by="ActivityCode")
-
-  
-  #New Column Just to identify the Source of data in table
+  #New Column just to identify the Source of data in table
   x_train$Source <- rep("Train",nrow(x_train)) 
-  
   
   #Add feature column names to Train
   colnames(x_train) <- c("RowId",features[2,],"ActivityCode","ActivityLabel","SubjectCode","DataSource")
-  
-  
-#   # Merge the Activty Names into the train data set 
-#   x_train <- merge(x_train,activity_labels, by="ActivityCode")
-  
-  
-  
-  # head(x_train)
   
 
   # ########################################################################################################
   #                                         Combine Test and Train 
   # ########################################################################################################   
   
-  # For Part 1, (as well as Part 3), all the data is in oned file and I have added the descriptive fields, but still need to make sure we have all numerical values in the data
+  # For Part 1, (as well as Part 3), all the data is in one file and I have added the descriptive fields, but still need to make sure we have all numerical values in the data
   x_combined <- rbind.data.frame(x_test,x_train)
   
   # For Part 2, only columns with mean or std
   x_mean_std <- subset(x_combined, select = x_combined[,c("RowId","SubjectCode","ActivityLabel", colnames(x_combined)[grep("mean()",colnames(x_combined))],colnames(x_combined)[grep("std()",colnames(x_combined))])])
   
-  #head(x_mean_std)
-                        
-  #x_subset <- subset(x_combined, select = c("ActivityLabel", "SubjectCode", colnames(x_combined %like% "mean()"), colnames(x_combined %like% "std()")))
-  
-  #head(x_subset)
-  
+  # Define user-friendly column headers to replace current ones
   names(x_mean_std) <- c("RowId",	"SubjectCode",	"ActivityLabel",
   "timeBodyAcceleration_Mean_X",	"timeBodyAcceleration_Mean_Y",	"timeBodyAcceleration_Mean_Z",
   "timeGravityAcceleration_Mean_X",	"timeGravityAcceleration_Mean_Y",	"timeGravityAcceleration_Mean_Z",
@@ -256,30 +196,31 @@ run_analysis <- function() {
   "frequencyBodyAccelerationMagnitude_StandardDeviation",	"frequencyBodyAccelerationJerkMagnitude_StandardDeviation",	"frequencyBodyGyroMagnitude_StandardDeviation",
   "frequencyBodyGyroJerkMagnitude_StandardDeviation" )
   
-
+  # Rename descriptive column names
   colsName <- names(x_mean_std)[2:3]
   
+  # Rename mean columns
   colsMean <- names(x_mean_std)[4:82]
 
-  
+  #Take the mean values
   x_mean_std.mean <- x_mean_std[,lapply(.SD,mean,na.rm=TRUE), by = colsName, .SDcols=colsMean] 
   
-
+  # Gather Volunteer (i.e. subject codes)
   x_mean_std.mean$SubjectCode <- factor(x_mean_std.mean$SubjectCode, levels =  c("1","2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21","22", "23", "24", "25", "26", "27", "28", "29", "30"), ordered = TRUE)
   
   x_mean_std.mean$SubjectCode <-  as.numeric(levels(x_mean_std.mean$SubjectCode)[x_mean_std.mean$SubjectCode]) 
   
+  # Order data by subject code for original data
+  # messydata <-   na.omit(x_mean_std[order(x_mean_std.mean$SubjectCode)])
   
- messydata <-   na.omit(x_mean_std[order(x_mean_std.mean$SubjectCode)])
+  # Order tidy data by volunteer number
+  tidydata <-   na.omit(x_mean_std.mean[order(x_mean_std.mean$SubjectCode)])
+
+  # Write tidy data to a file
+  write.table(tidydata, file = "975119_getdata-035.txt", row.names = FALSE)
   
-  
- tidydata <-   na.omit(x_mean_std.mean[order(x_mean_std.mean$SubjectCode)])
+  print("process complete!")
 
- 
- write.table(tidydata, file = "975119_getdata-035.txt", row.names = FALSE)
-
-
- 
   
 }
 
